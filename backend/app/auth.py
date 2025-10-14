4dcf30628e54d47a630bf71ea8c8b9ce14424f6e
TEPPA122 <maksim2021bese@gmail.com>
2025-10-14 20:33:32 +0200

    Создание JWT Token, (проблемы с библиотекой)

---
+++ backend/app/auth.py
@@ -0,0 +1,14 @@
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+
+# Creating a JWT token
+SECRET_KEY = "секрет_строка_для_подписи" 
+ALGORITHM = "HS256" 
+ACCESS_TOKEN_EXPIRE_MINUTES = 30 
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now(datetime.timezone.utc)() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES) #
+    to_encode.update({"exp": expire}) 
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt 
---
+++ backend/app/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app import models, schemas, database
+from app.auth import create_access_token
+from app.utils import verify_password
+
+app = FastAPI()
+
+@app.post("/login")
+def login(user: schemas.UserLogin, db: Session = Depends(database.get_db)):
+    db_user = db.query(models.User).filter(models.User.email == user.email).first()
+    if not db_user:
+        raise HTTPException(status_code=400, detail="User not found")
+
+    if not verify_password(user.password, db_user.hashed_password):
+        raise HTTPException(status_code=400, detail="Incorrect password")
+
+    # создание JWT токена
+    access_token = create_access_token(data={"sub": str(db_user.id)})
+
+    return {"access_token": access_token, "token_type": "bearer"}
