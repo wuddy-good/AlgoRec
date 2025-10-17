2ddbacd26d7063fcb387637e5b4255e37e6e31ee
Vlad Polishchuk <vladpolisuk127@gmail.com>
2025-10-17 20:12:08 +0300

    style: PEP8 format

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,13 +9,9 @@ app = FastAPI()
 @app.post("/login")
 def login(user: schemas.UserLogin, db: Session = Depends(database.get_db)):
     db_user = db.query(models.User).filter(models.User.email == user.email).first()
-    if not db_user:
-        raise HTTPException(status_code=400, detail="User not found")
+    if not db_user or not verify_password(user.password, db_user.hashed_password):
+        raise HTTPException(status_code=401, detail="Invalid email or password")
 
-    if not verify_password(user.password, db_user.hashed_password):
-        raise HTTPException(status_code=400, detail="Incorrect password")
-
-    # создание JWT токена
     access_token = create_access_token(data={"sub": str(db_user.id)})
 
     return {"access_token": access_token, "token_type": "bearer"}
--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 
-# Schema for user registration
+
 class UserLogin(BaseModel):
     email: str
     password: str
