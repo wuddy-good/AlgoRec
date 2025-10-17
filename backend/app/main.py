1fd1b05336003dfec74989444c363f5a10a75a9d
Vlad Polishchuk <vladpolisuk127@gmail.com>
2025-10-17 20:15:09 +0300

    style: PEP8 fix and delted not needed main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,14 +4,21 @@ from app import models, schemas, database
 from app.auth import create_access_token
 from app.utils import verify_password
 
+
 app = FastAPI()
+
 
 @app.post("/login")
 def login(user: schemas.UserLogin, db: Session = Depends(database.get_db)):
     db_user = db.query(models.User).filter(models.User.email == user.email).first()
     if not db_user or not verify_password(user.password, db_user.hashed_password):
-        raise HTTPException(status_code=401, detail="Invalid email or password")
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
 
-    access_token = create_access_token(data={"sub": str(db_user.id)})
+    access_token = create_access_token(
+        data={"sub": str(db_user.id)}
+    )
 
     return {"access_token": access_token, "token_type": "bearer"}
