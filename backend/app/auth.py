33ce760debeae33ed728d9762c0cbf78a9afd595
Vlad Polishchuk <vladpolisuk127@gmail.com>
2025-10-17 20:12:08 +0300

    style: PEP8 format

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -1,14 +1,17 @@
-from datetime import datetime, timedelta
-from jose import jwt, JWTError
+from datetime import datetime, timedelta, timezone
+from jose import jwt  # JWTError можна додати, якщо треба для обробки помилок
 
-# Creating a JWT token
-SECRET_KEY = "секрет_строка_для_подписи" 
-ALGORITHM = "HS256" 
-ACCESS_TOKEN_EXPIRE_MINUTES = 30 
+# Set up
+SECRET_KEY = "секрет_строка_для_подписи"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
 
 def create_access_token(data: dict):
+    """Створює JWT-токен з терміном дії."""
     to_encode = data.copy()
-    expire = datetime.now(datetime.timezone.utc)() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES) #
-    to_encode.update({"exp": expire}) 
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt 
+    return encoded_jwt
