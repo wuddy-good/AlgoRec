298f6cd6cf848b140a49ab71af68bcb190995a0e
Vlad Polishchuk <vladpolisuk127@gmail.com>
2025-10-17 20:16:37 +0300

    style: fix main.py for PEP8

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,8 +10,14 @@ app = FastAPI()
 
 @app.post("/login")
 def login(user: schemas.UserLogin, db: Session = Depends(database.get_db)):
-    db_user = db.query(models.User).filter(models.User.email == user.email).first()
-    if not db_user or not verify_password(user.password, db_user.hashed_password):
+    db_user = (
+        db.query(models.User)
+        .filter(models.User.email == user.email)
+        .first()
+    )
+    if not db_user or not verify_password(
+        user.password, db_user.hashed_password
+    ):
         raise HTTPException(
             status_code=401,
             detail="Invalid email or password"
@@ -21,4 +27,7 @@ def login(user: schemas.UserLogin, db: Session = Depends(database.get_db)):
         data={"sub": str(db_user.id)}
     )
 
-    return {"access_token": access_token, "token_type": "bearer"}
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
