d4aa84daa8ea96fcee1b88643d81bc5be1247a68
TEPPA122 <maksim2021bese@gmail.com>
2025-10-29 22:00:48 +0100

    +Added User DB

---
+++ backend/app/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.sql import func
+from app.base import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(255), unique=True, index=True, nullable=False)
+    hashed_password = Column(String(255), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    location = Column(String(255), nullable=False)
